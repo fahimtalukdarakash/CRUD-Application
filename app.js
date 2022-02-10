0cb1667e7f1ba8e6beca0677f640d5dbf3d4770f
fahimtalukdarakash <77202043+fahimtalukdarakash@users.noreply.github.com>
2022-02-11 03:14:51 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+const express=require('express');
+const exphbs=require('express-handlebars');
+const bodyParser=require('body-parser');
+const mysql=require('mysql');
+require('dotenv').config();
+const app=express();
+const port=process.env.PORT || 5000;
+//parsing middleware
+app.use(bodyParser.urlencoded({extended: false}));
+//parse application json
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.engine('hbs', exphbs.engine({extname:'.hbs'}));
+app.set('view engine', 'hbs');
+const pool =mysql.createPool({
+  connectionLimit: 100,
+  host           : process.env.DB_HOST,
+  user           : process.env.DB_USER,
+  password       : process.env.DB_PASS,
+  database       : process.env.DB_NAME
+});
+
+pool.getConnection((err,connection) => {
+  if(err) throw err;
+  console.log('Connected as ID' + connection.threadId);
+});
+
+
+const routes = require('./server/routes/user');
+app.use('/',routes);
+
+app.listen(port,()=> console.log(`Listening on port ${port}`));
